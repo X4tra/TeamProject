e0427e77cc61de3b01a0697f34a5d61601a356cb
X4tra <x4tra@localhost.localdomain>
2026-06-17 20:10:43 +0200

    Added saving test results to a file

--- test/test.py
+++ test/test.py
@@ -3,3 +3,6 @@ import requests
 # Simplest GET request to the 'api' container
 response = requests.get("http://dashboard:8000")
 print(response)
+
+with open("test_output.txt", "w") as f:
+    f.write(f"Response from dashboard: {response.text}\n")
