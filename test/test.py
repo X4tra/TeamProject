68d74f91e504a7d7713cde660420cf57494fc753
X4tra <x4tra@localhost.localdomain>
2026-06-18 22:32:39 +0200

    Final fix for testing API

--- test/test.py
+++ test/test.py
@@ -1,8 +1,5 @@
 import requests
 
 # Simplest GET request to the 'api' container
-response = requests.get("http://dashboard:8000")
-print(response)
-
-with open("test_output.txt", "w") as f:
-    f.write(f"Response from dashboard: {response.text}\n")
+response = requests.get("http://dashboard:8000/api/test")
+print(response.text)
